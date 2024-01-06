8e7dec794d803532af842d87f3cd553463aec9b2
Bibash Roy <bibhash@gmail.com>
2024-01-06 12:39:06 +0530

    done sidebar

--- src/components/ConversationsItem.jsx
+++ src/components/ConversationsItem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function ConversationsItem({props}) {
   return (
     <div className="conversation-container">
-      <p className='conversation-icon'> {props.name[0]}</p>
+      <p className='con-icon'> {props.name[0]}</p>
       <p className='con-title'>{props.name}</p>
       <p className='con-lastMessage'>{props.lastMessage}</p>
       <p className='con-timeStamp'>{props.timeStamp}</p>
